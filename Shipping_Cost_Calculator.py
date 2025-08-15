ac6e4347c8dc42b48bf21d3bf8d4024e5b4369b5
Pranavi <pranavimanikonda@gmail.com>
2025-08-15 17:07:58 +0530

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,1 +1,8 @@
- Here is a new update by <Pranavi16-star>
+# Here is a new update by <Pranavi16-star>
+git add .
+ git commit -m "added additional line to Shipping_Cost_Calculator.py"
+git revert HEAD --no-edit
+git checkout main
+git merge shipping_calculator_fixes
+git push origin main
+git log
